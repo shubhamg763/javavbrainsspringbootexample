674b42c248ff2def63a38a9a1109b53ff36dc6fe
Shubham Gupta <shubhamg763@gmail.com>
2020-09-02 13:19:54 +0530

    First commit

---
+++ course-api/src/main/java/sample/courseapi/hello/HelloController.java
@@ -0,0 +1,15 @@
+package sample.courseapi.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi(){
+		
+		return "Hello World";
+	}
+
+}
